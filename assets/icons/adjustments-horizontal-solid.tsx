ace8f3ec5c19a7b581ff0b0a14d4d3ed5f80a3a8
gh-kimcharles <hau.kmdeguzman1@gmail.com>
2026-05-15 20:02:31 +0800

    style: update HeroSection icon with Icon components (/assets/*.tsx)

---
+++ assets/icons/adjustments-horizontal-solid.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+type IconProps = React.SVGProps<SVGSVGElement> & {
+  size?: number;
+};
+
+export const AdjustmentsHorizontalSolid = ({
+  size = 24,
+  className,
+  ...props
+}: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      width={size}
+      height={size}
+      className={className}
+      {...props}
+    >
+      <path d="M18.75 12.75h1.5a.75.75 0 0 0 0-1.5h-1.5a.75.75 0 0 0 0 1.5ZM12 6a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 12 6ZM12 18a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 12 18ZM3.75 6.75h1.5a.75.75 0 1 0 0-1.5h-1.5a.75.75 0 0 0 0 1.5ZM5.25 18.75h-1.5a.75.75 0 0 1 0-1.5h1.5a.75.75 0 0 1 0 1.5ZM3 12a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 3 12ZM9 3.75a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM12.75 12a2.25 2.25 0 1 1 4.5 0 2.25 2.25 0 0 1-4.5 0ZM9 15.75a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Z" />
+    </svg>
+  );
+};
---
+++ assets/icons/index.ts
@@ -0,0 +1,10 @@
+export { LinkSolid } from "./link-solid";
+
+export { CloudArrowDownSolid } from "./cloud-arrow-down-solid";
+
+export { InboxArrowDownSolid } from "./inbox-arrow-down-solid";
+
+export { WrenchScrewdriverSolid } from "./wrench-screwdriver-solid";
+export { RectangleGroupSolid } from "./rectangle-stack-solid";
+export { Square3Stack3dSolid } from "./square-3-stack-3d-solid";
+export { AdjustmentsHorizontalSolid } from "./adjustments-horizontal-solid";
